6769a9dcc817ce017796bdc53e5601ee837f287c
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2022-08-03 12:49:56 +0300

    Generate a more portable executable with nuitka

--- nuitka_build.sh
+++ nuitka_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm wookiee_broadcaster
-nuitka3 wookiee_broadcaster.py --follow-imports --remove-output
+nuitka3 wookiee_broadcaster.py --standalone --onefile --remove-output
 mv wookiee_broadcaster.bin wookiee_broadcaster
 
