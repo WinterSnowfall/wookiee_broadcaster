6c805c5e88fa98e58a37a6583bdb1ea918cc2a5e
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-10-19 20:20:52 +0300

    Remove Nuitka build script

--- nuitka_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NUITKA_COMPILE_FLAGS="--standalone --onefile --remove-output --no-deployment-flag=self-execution"
-
-rm bin/wookiee_broadcaster
-nuitka3 wookiee_broadcaster.py $NUITKA_COMPILE_FLAGS
-chmod +x wookiee_broadcaster.bin
-mv wookiee_broadcaster.bin bin/wookiee_broadcaster
-
